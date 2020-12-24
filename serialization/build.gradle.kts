452e9969c1f3449c642715aa37ead6d79667671a
Maksim_Likhanov <LikhanovMaxim@yandex.ru>
2020-12-24 18:22:01 +0300

    update kotlin

--- serialization/build.gradle.kts
+++ serialization/build.gradle.kts
@@ -4,18 +4,21 @@ plugins {
     id("kotlinx-atomicfu")
     application
 }
-
+//apply plugin: 'kotlinx-serialization'
 application {
     mainClassName = "com.HelloKt" //TODO package
 }
 
+val serializationVersion:String by project
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
     //provided by drill runtime
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor")
+//    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime")
+//    implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$serializationVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
     //provided by admin
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable-jvm")
 
